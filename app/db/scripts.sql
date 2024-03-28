d048525f48d0e8c8974eed2ed40128622f64bc44
phamlap <lapb2105580@student.ctu.edu.vn>
2024-03-28 10:21:18 +0700

    chore: update tables

--- app/db/scripts.sql
+++ app/db/scripts.sql
@@ -1,59 +1,57 @@
-create database 'high-school-management';
+create database if not exists high_school_management;
 
-create table class (
+use high_school_management;
+
+create table if not exists academic_years (
+  academic_year char(9) primary key
+);
+create table if not exists classes (
   class_id int primary key auto_increment,
   class_name varchar(10) not null,
   academic_year char(9) not null references academic_year(academic_year)
 );
-
-create table teacher (
+create table if not exists teachers (
   teacher_id int primary key auto_increment,
   full_name varchar(255) not null,
   date_of_birth date not null,
   address varchar(255) not null,
   phone_number char(10) not null
 );
-
-create table student (
+create table if not exists students (
   student_id int primary key auto_increment,
   full_name varchar(255) not null,
   date_of_birth date not null,
   address varchar(255) not null,
   parent_phone_number char(10) not null,
   class_id int not null,
-  foreign key (class_id) references class(class_id)
+  foreign key (class_id) references classes(class_id)
 );
-
-create table homeroom_teacher (
+create table if not exists homeroom_teachers (
   teacher_id int not null,
   class_id int not null,
   primary key (teacher_id, class_id),
-  foreign key (teacher_id) references teacher(teacher_id),
-  foreign key (class_id) references class(class_id)
+  foreign key (teacher_id) references teachers(teacher_id),
+  foreign key (class_id) references classes(class_id)
 );
-
-create table room (
+create table if not exists rooms (
   room_id int primary key auto_increment,
   room_number varchar(50) not null,
   maximum_capacity int not null
 );
-
-create table room_class (
+create table if not exists room_class (
   room_id int not null,
   class_id int not null,
   semester tinyint not null,
   primary key (room_id, class_id, semester),
-  foreign key (room_id) references room(room_id),
-  foreign key (class_id) references class(class_id)
+  foreign key (room_id) references rooms(room_id),
+  foreign key (class_id) references classes(class_id)
 );
-
-create table subject (
+create table if not exists subjects (
   subject_id int primary key auto_increment,
   subject_name varchar(255) not null,
   grade tinyint not null
 );
-
-create table mark (
+create table if not exists marks (
   student_id int not null,
   subject_id int not null,
   oral_score decimal(4, 2) not null,
@@ -61,28 +59,21 @@ create table mark (
   1_period_score decimal(4, 2) not null,
   semester_score decimal(4, 2) not null,
   primary key (student_id, subject_id),
-  foreign key (student_id) references student(student_id),
-  foreign key (subject_id) references subject(subject_id)
+  foreign key (student_id) references students(student_id),
+  foreign key (subject_id) references subjects(subject_id)
 );
-
-create table academic_year (
-  academic_year char(9) primary key
-);
-
-create table teaching (
+create table if not exists teaching (
   teacher_id int not null,
   subject_id int not null,
   semester tinyint not null,
   academic_year char(9) not null,
   primary key (teacher_id, subject_id, semester, academic_year),
-  foreign key (teacher_id) references teacher(teacher_id),
-  foreign key (subject_id) references subject(subject_id),
-  foreign key (academic_year) references academic_year(academic_year)
+  foreign key (teacher_id) references teachers(teacher_id),
+  foreign key (subject_id) references subjects(subject_id),
+  foreign key (academic_year) references academic_years(academic_year)
 );
-
-create table user (
+create table if not exists users (
   email varchar(50) primary key,
   password varchar(255) not null,
-  role varchar(10) not null,
+  role varchar(10) not null
 );
--- role: admin, teacher
